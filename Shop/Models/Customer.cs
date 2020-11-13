af1c48d379252cb27f3a072570c051d11d35ca0c
ทศพล สนธิพิณ <thodsaphonSP@hotmail.co.th>
2020-11-13 10:21:32 +0700

    Add project files.

---
+++ Shop/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string lastname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Provine { get; set; }
+        public string City { get; set; }
+        public List<Order> Order { get; set; }
+    }
+
+    public class Order
+    {
+        private DateTime OrderDateTime { get; set; }
+    }
+}
